2f490f789ac6b2f3ac1a580ab915222a76303311
MEHRDADsmj <59224497+MEHRDADsmj@users.noreply.github.com>
2022-11-18 13:10:23 +0330

    Implemented level data SO

---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LevelData", fileName = "Level ")]
+public class LevelData : ScriptableObject
+{
+    [SerializeField] private List<TileInfo> tiles;
+}
+
+[Serializable]
+struct TileInfo
+{
+    [SerializeField] [Tooltip("Voxel coordinates of the tile stack. Player always starts at (0, 0)")]
+    private TileCoord coords;
+    [SerializeField] [Tooltip("A value of 1 means base height")] private int height;
+    [SerializeField] private bool isGoal;
+}
+
+[Serializable]
+struct TileCoord
+{
+    [SerializeField] private int x;
+    [SerializeField] private int y;
+
+    public int X
+    {
+        get { return x; }
+    }
+
+    public int Y
+    {
+        get { return y; }
+    }
+}
